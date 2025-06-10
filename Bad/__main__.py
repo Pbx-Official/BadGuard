5a4b577f5f3b1e6f72f373a0cc7215e53514c73b
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-06-10 23:04:58 +0530

    Update and rename main.py to __main__.py

--- Bad/__main__.py
+++ Bad/__main__.py
@@ -24,8 +24,13 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("Bad.plugins" + all_module)
-    LOGGER("Bad.plugins").info("Successfully Imported Modules...")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("Bad.plugins").info("Successfully Imported All Modules ")
+    
     await Bad.start()
     await application.run_polling()
     await application.start()
