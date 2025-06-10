d1316e39d594ec37f97d98fa4c6c3ad763c7a097
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-06-10 21:40:40 +0530

    Delete BADMUNDA directory

--- BADMUNDA/__main__.py
+++
@@ -1,8 +0,0 @@
-import uvloop  # Comment it out if using on windows
-from BADMUNDA.bot_class import BAD
-
-if __name__ == "__main__":
-    uvloop.install() # Comment it out if using on windows
-    BAD().run()
-    
-    
