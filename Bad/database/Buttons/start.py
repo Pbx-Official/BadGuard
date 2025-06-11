56b6d0e7569b945718a74c0d63f90d7a6d388dd4
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-06-11 23:27:42 +0530

    Update start.py

--- Bad/database/Buttons/start.py
+++ Bad/database/Buttons/start.py
@@ -1,23 +1,17 @@
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from config import SUPPORT_GROUP
 from Bad import app
 
-def start_pannel(_):
+
+
+def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text="۞ 𝐇𝙴𝙻𝙿 ۞", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-        ],
-        [
-            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
-            InlineKeyboardButton(text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"),
-        ],
-        [
-            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -27,31 +21,19 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                text="۞ 𝐇𝙴𝙻𝙿 ۞",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_GROUP),
-            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(
-                text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
-            )
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", url=config.UPSTREAM_REPO),
         ],
+        [InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper")],
     ]
     return buttons
-
-
-def alive_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
-        ],
-    ]
-    return buttons
-
+  
