91e5edd6527c1a482b588876b313cb7646b8912b
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-06-12 15:16:02 +0530

    Update __init__.py

--- Bad/database/__init__.py
+++ Bad/database/__init__.py
@@ -2,3 +2,29 @@ from .sysdb import *
 from .filtersdb import *
 from .extractiondb import *
 from .formattersdb import *
+
+
+ZeroTwo = [
+    "Zero1",
+    "Zero2",
+    "Zero3",
+    "Zero4",
+    "Zero5",
+    "Zero6",
+    "Zero7",
+    "Zero8",
+    "Zero9",
+    "Zero10",
+    "Zero11",
+    "Zero12",
+    "Zero13",
+    "Zero14",
+    "Zero15",
+    "Zero16",
+    "Zero17",
+    "Zero18",
+    "Zero19",
+    "Zero20",
+    "Zero21",
+    "Zero22",
+]
