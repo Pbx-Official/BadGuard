5214678ca8843220c5f2f3a65be1f6b9f521b381
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-06-12 19:03:06 +0530

    Update database.py

--- Bad/database/database.py
+++ Bad/database/database.py
@@ -696,24 +696,3 @@ async def add_off(on_off: int):
         return
     return await onoffdb.delete_one({"on_off": on_off})
     
-
-
-async def blacklisted_chats() -> list:
-    chats_list = []
-    async for chat in blacklist_chatdb.find({"chat_id": {"$lt": 0}}):
-        chats_list.append(chat["chat_id"])
-    return chats_list
-
-
-async def blacklist_chat(chat_id: int) -> bool:
-    if not await blacklist_chatdb.find_one({"chat_id": chat_id}):
-        await blacklist_chatdb.insert_one({"chat_id": chat_id})
-        return True
-    return False
-
-
-async def whitelist_chat(chat_id: int) -> bool:
-    if await blacklist_chatdb.find_one({"chat_id": chat_id}):
-        await blacklist_chatdb.delete_one({"chat_id": chat_id})
-        return True
-    return False
