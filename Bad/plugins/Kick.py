283b7316826cada83da3938376d28fe1fc75ec79
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-06-12 19:11:01 +0530

    Update Kick.py

--- Bad/plugins/Kick.py
+++ Bad/plugins/Kick.py
@@ -5,7 +5,7 @@ from pyrogram.errors.exceptions.bad_request_400 import (
     BadRequest
 )
 import requests
-from pbx import Owner
+from Pbx import Owner
 from Bad import app, LOGGER
 from Bad.misc import SUDOERS
 from pyrogram.errors import UserNotParticipant, PeerIdInvalid
