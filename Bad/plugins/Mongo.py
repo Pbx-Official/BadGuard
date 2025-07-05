9117338ae20c854c52d4ddd096ff8a0bb0c93a8e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-07-05 18:38:20 +0530

    Update Mongo.py

--- Bad/plugins/Mongo.py
+++ Bad/plugins/Mongo.py
@@ -6,7 +6,7 @@ from Bad import app
 import os
 from config import OWNER_ID
 from Bad.misc import SUDOERS
-from Bad.utils.pastebin import BADbin
+from Bad.database.pastebin import BaDbin
 
 
 MONGO_DB_URI = os.getenv("MONGO_DB_URI")
